7e0947a790448da14b35ff6bc4b7f2fe186dd669
Cordar <enricmir1993@gmail.com>
2023-10-27 08:08:11 +0200

    add ability system base classes and enabled plugin

---
+++ Source/HeartWeaver/Public/AbilitySystem/KDSAbilitySystemComponent.h
@@ -0,0 +1,17 @@
+// Copyright Kraken's Den Studios. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "KDSAbilitySystemComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HEARTWEAVER_API UKDSAbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/HeartWeaver/Public/AbilitySystem/KDSAttributeSet.h
@@ -0,0 +1,17 @@
+// Copyright Kraken's Den Studios. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "KDSAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HEARTWEAVER_API UKDSAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
