74947a7026ddf3a2283254060160b0fb4513317d
Cordar <enricmir1993@gmail.com>
2023-10-27 08:05:30 +0200

    adds players state

---
+++ Source/HeartWeaver/Private/Player/KDSPlayerState.cpp
@@ -0,0 +1,9 @@
+// Copyright Kraken's Den Studios. All rights reserved.
+
+
+#include "Player/KDSPlayerState.h"
+
+AKDSPlayerState::AKDSPlayerState()
+{
+	NetUpdateFrequency = 100.f;
+}
---
+++ Source/HeartWeaver/Public/Player/KDSPlayerState.h
@@ -0,0 +1,19 @@
+// Copyright Kraken's Den Studios. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "KDSPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HEARTWEAVER_API AKDSPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+public:
+	AKDSPlayerState();
+	
+};
