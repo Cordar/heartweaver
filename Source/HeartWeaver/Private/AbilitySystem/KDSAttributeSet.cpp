d408ab51fb90aae830e1e5ffc59d4d8a7bd18442
Cordar <enricmir1993@gmail.com>
2023-12-11 12:45:05 +0100

    returned to default values

--- Source/HeartWeaver/Private/AbilitySystem/KDSAttributeSet.cpp
+++ Source/HeartWeaver/Private/AbilitySystem/KDSAttributeSet.cpp
@@ -3,3 +3,28 @@
 
 #include "AbilitySystem/KDSAttributeSet.h"
 
+#include "Net/UnrealNetwork.h"
+
+UKDSAttributeSet::UKDSAttributeSet()
+{
+	InitHealth(1.f);
+	InitMaxHealth(1.f);
+}
+
+void UKDSAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UKDSAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UKDSAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+}
+
+void UKDSAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UKDSAttributeSet, Health, OldHealth);
+}
+
+void UKDSAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UKDSAttributeSet, MaxHealth, OldMaxHealth);
+}
--- Source/HeartWeaver/Public/AbilitySystem/KDSAttributeSet.h
+++ Source/HeartWeaver/Public/AbilitySystem/KDSAttributeSet.h
@@ -3,8 +3,17 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
 #include "AttributeSet.h"
+
 #include "KDSAttributeSet.generated.h"
+
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  * 
@@ -13,5 +22,21 @@ UCLASS()
 class HEARTWEAVER_API UKDSAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
-	
+public:
+	UKDSAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes")
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UKDSAttributeSet, Health);
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UKDSAttributeSet, MaxHealth);
+
+	UFUNCTION()
+	virtual void OnRep_Health(const FGameplayAttributeData& OldHealth);
+
+	UFUNCTION()
+	virtual void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth);
 };
