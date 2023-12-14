7174721f10e8a3de3d0a535aa64e0843128a5531
Cordar <enricmir1993@gmail.com>
2023-12-14 12:50:41 +0100

    added durationgeclass

--- Source/HeartWeaver/Public/Actor/KrakenEffectActor.h
+++ Source/HeartWeaver/Public/Actor/KrakenEffectActor.h
@@ -25,4 +25,7 @@ protected:
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
 	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
+	TSubclassOf<UGameplayEffect> DurationGameplayEffectClass;
 };
