b3a3fa41905a486f2c01c8aaa9b0222d91757e3c
Cordar <enricmir1993@gmail.com>
2023-12-14 19:01:50 +0100

    added trace climbable surfaces functions

--- Source/HeartWeaver/Private/Characters/KrakenCharacterMovementComponent.cpp
+++ Source/HeartWeaver/Private/Characters/KrakenCharacterMovementComponent.cpp
@@ -3,6 +3,8 @@
 
 #include "Characters/KrakenCharacterMovementComponent.h"
 
+#include "Kismet/KismetSystemLibrary.h"
+
 UKrakenCharacterMovementComponent::UKrakenCharacterMovementComponent()
 {
 }
@@ -23,4 +25,41 @@ float UKrakenCharacterMovementComponent::GetMaxSpeed() const
 {
 	// TODO: Implement for ability system tag movement stopped to be 0 (check Lyra Project)
 	return Super::GetMaxSpeed();
+}
+
+void UKrakenCharacterMovementComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	TraceClimbableSurfaces();
+}
+
+TArray<FHitResult> UKrakenCharacterMovementComponent::DoCapsuleTraceMultiByObject(const FVector& Start, const FVector& End, bool bShowDebugShape)
+{
+	TArray<FHitResult> OutCapsuleTraceHitResults;
+	
+	UKismetSystemLibrary::CapsuleTraceMultiForObjects(
+		this,
+		Start,
+		End,
+		ClimbCapsuleTraceRadius,
+		ClimbCapsuleTraceHalfHeight,
+		ClimbableSurfaceTraceTypes,
+		false,
+		TArray<AActor*>(),
+		bShowDebugShape? EDrawDebugTrace::ForOneFrame : EDrawDebugTrace::None,
+		OutCapsuleTraceHitResults,
+		false
+	);
+
+	return OutCapsuleTraceHitResults;
+}
+
+void UKrakenCharacterMovementComponent::TraceClimbableSurfaces()
+{
+	const FVector StartOffset = UpdatedComponent->GetForwardVector() * 30.f;
+	const FVector Start = UpdatedComponent->GetComponentLocation() + StartOffset;
+	const FVector End = Start + UpdatedComponent->GetForwardVector();
+	
+	DoCapsuleTraceMultiByObject(Start, End, true);
 }
--- Source/HeartWeaver/Public/Characters/KrakenCharacterMovementComponent.h
+++ Source/HeartWeaver/Public/Characters/KrakenCharacterMovementComponent.h
@@ -21,6 +21,26 @@ public:
 	//~UMovementComponent interface
 	virtual FRotator GetDeltaRotation(float DeltaTime) const override;
 	virtual float GetMaxSpeed() const override;
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 	//~End of UMovementComponent interface
+
+protected:
+	//~Climb Variables
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Character Movement: Climbing")
+	TArray<TEnumAsByte<EObjectTypeQuery> > ClimbableSurfaceTraceTypes;
 	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Character Movement: Climbing")
+	float ClimbCapsuleTraceRadius = 50.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Character Movement: Climbing")
+	float ClimbCapsuleTraceHalfHeight = 72.f;
+	//~End of Climb Variables
+	
+private:
+	//~Climb Traces
+	TArray<FHitResult> DoCapsuleTraceMultiByObject(const FVector& Start, const FVector& End, bool bShowDebugShape = false);
+	//~End of ClimbTraces
+
+	//~Climb Core
+	void TraceClimbableSurfaces();
 };
