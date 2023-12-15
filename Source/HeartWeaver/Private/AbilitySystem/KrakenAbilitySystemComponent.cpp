f617f8d902e2c6fca8234382479dd02484ea3b22
Cordar <enricmir1993@gmail.com>
2023-12-15 13:06:47 +0100

    m

--- Source/HeartWeaver/Private/AbilitySystem/KrakenAbilitySystemComponent.cpp
+++ Source/HeartWeaver/Private/AbilitySystem/KrakenAbilitySystemComponent.cpp
@@ -16,6 +16,8 @@ void UKrakenAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* Abili
 	EffectSpec.GetAllAssetTags(TagContainer);
 	for (const FGameplayTag& Tag: TagContainer)
 	{
-		
+		// TODO: Broadcast the tag to the Widget Controller
+		const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
+		GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Blue, Msg);
 	}
 }
