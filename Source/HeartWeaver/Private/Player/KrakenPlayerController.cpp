eb2980af2ffcc0ae0331b2360ef3a451e0e44e1d
Cordar <enricmir1993@gmail.com>
2024-06-03 21:44:45 +0200

    walk on walls, fix compile

--- Source/HeartWeaver/Private/Player/KrakenPlayerController.cpp
+++ Source/HeartWeaver/Private/Player/KrakenPlayerController.cpp
@@ -17,6 +17,79 @@ AKrakenPlayerController::AKrakenPlayerController()
 	bReplicates = true;
 }
 
+void AKrakenPlayerController::UpdateRotation(const float DeltaTime)
+{
+	FVector GravityDirection = FVector::DownVector;
+	if (const ACharacter* PlayerCharacter = Cast<ACharacter>(GetPawn()))
+	{
+		if (UCharacterMovementComponent* MoveComp = PlayerCharacter->GetCharacterMovement())
+		{
+			GravityDirection = MoveComp->GetGravityDirection();
+		}
+	}
+ 
+	// Get the current control rotation in world space
+	FRotator ViewRotation = GetControlRotation();
+ 
+	// Add any rotation from the gravity changes, if any happened.
+	// Delete this code block if you don't want the camera to automatically compensate for gravity rotation.
+	if (!LastFrameGravity.Equals(FVector::ZeroVector))
+	{
+		const FQuat DeltaGravityRotation = FQuat::FindBetweenNormals(LastFrameGravity, GravityDirection);
+		const FQuat WarpedCameraRotation = DeltaGravityRotation * FQuat(ViewRotation);
+ 
+		ViewRotation = WarpedCameraRotation.Rotator();	
+	}
+	LastFrameGravity = GravityDirection;
+ 
+	// Convert the view rotation from world space to gravity relative space.
+	// Now we can work with the rotation as if no custom gravity was affecting it.
+	ViewRotation = GetGravityRelativeRotation(ViewRotation, GravityDirection);
+ 
+	// Calculate Delta to be applied on ViewRotation
+	FRotator DeltaRot(RotationInput);
+ 
+	if (PlayerCameraManager)
+	{
+		ACharacter* PlayerCharacter = Cast<ACharacter>(GetPawn());
+ 
+		PlayerCameraManager->ProcessViewRotation(DeltaTime, ViewRotation, DeltaRot);
+ 
+		// Zero the roll of the camera as we always want it horizontal in relation to the gravity.
+		ViewRotation.Roll = 0;
+ 
+		// Convert the rotation back to world space, and set it as the current control rotation.
+		SetControlRotation(GetGravityWorldRotation(ViewRotation, GravityDirection));
+	}
+
+	if (APawn* const P = GetPawnOrSpectator())
+	{
+		P->FaceRotation(ViewRotation, DeltaTime);
+	}
+}
+
+FRotator AKrakenPlayerController::GetGravityRelativeRotation(const FRotator& Rotation, const FVector& GravityDirection)
+{
+	if (!GravityDirection.Equals(FVector::DownVector))
+	{
+		const FQuat GravityRotation = FQuat::FindBetweenNormals(GravityDirection, FVector::DownVector);
+		return (GravityRotation * Rotation.Quaternion()).Rotator();
+	}
+ 
+	return Rotation;
+}
+
+FRotator AKrakenPlayerController::GetGravityWorldRotation(const FRotator& Rotation, const FVector& GravityDirection)
+{
+	if (!GravityDirection.Equals(FVector::DownVector))
+	{
+		const FQuat GravityRotation = FQuat::FindBetweenNormals(FVector::DownVector, GravityDirection);
+		return (GravityRotation * Rotation.Quaternion()).Rotator();
+	}
+ 
+	return Rotation;
+}
+
 void AKrakenPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
