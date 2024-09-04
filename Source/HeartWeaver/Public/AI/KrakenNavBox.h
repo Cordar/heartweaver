50e1055833b9806897e9d2190e15ba7bddda1785
Chipi <61060509+ChipiDev@users.noreply.github.com>
2024-09-04 13:01:53 +0200

    Custom pathfinding and AI Nav Mesh (#394)
    * Hotfix Aim Mark: Ahora al hacer clic derecho el mouse aparece sobre el jugador
    
    * Primera versión de generación de mesh customizada
    
    * Optimización del nav mesh
    
    * NavMesh con funcionamiento básico con boxes
    
    * Soporte de Blocking volume a custom nav mesh
    
    * Mejorado cálculo de colisiones del nav mesh para objetos rotados
    
    * Commit previo al cambio de números enteros en Nav Mesh
    
    * Añadido radio de evitación a NavMesh
    
    * Pathfinding básico escupido por chatGPT
    
    * Avance previo a la finalización del movimiento de los bichos usando pathfinding
    
    * Movimiento de IA con Pathfinding básico
    
    * Desarollo de la IA: Movimiento hacia target
    
    * Percepción de IA Básica
    
    * Optimización de pathfinding
    
    * Retoque final para IA Buyublin
    
    * Árbol de AI Básico de buyuglin

---
+++ Source/HeartWeaver/Public/AI/KrakenNavBox.h
@@ -0,0 +1,139 @@
+// Copyright Kraken's Den Studios. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "KrakenNavBox.generated.h"
+
+// struct FNavMeshVoxelInfo;
+
+struct FKSphereElem;
+struct FKBoxElem;
+class UBoxComponent;
+
+UCLASS()
+class HEARTWEAVER_API AKrakenNavBox : public AActor
+{
+	GENERATED_BODY()
+
+	bool bCalculatingNavMesh = false;
+
+	UPROPERTY()
+	USceneComponent* Root;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, meta = (AllowPrivateAccess), Category= "Grid")
+	float AgentHeight;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, meta = (AllowPrivateAccess), Category= "Grid")
+	float AvoidanceRadius;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, meta = (AllowPrivateAccess), Category= "Grid")
+	bool bUpdateNavMesh = false;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, meta = (AllowPrivateAccess), Category= "Grid")
+	bool bGenerateOnlyFloorLevel = true;
+
+	float DesignedZAnalisisLevel;
+
+	UPROPERTY()
+	TSet<FVector> GridMap;
+
+	// UPROPERTY(BlueprintReadOnly, EditAnywhere, meta = (AllowPrivateAccess), Category= "Grid")
+	TSet<FVector> TestGridMap;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, meta = (AllowPrivateAccess), Category= "Grid")
+	float GridDistance;
+
+	// UPROPERTY(BlueprintReadOnly, VisibleAnywhere, meta = (AllowPrivateAccess), Category= "Grid")
+	TArray<FVector> LinealGrid;
+
+	// Array creado aquí para ahorrar memoria
+	TSet<FVector> LocalGridMap;
+	TSet<FVector> TempLocalGridMap;
+
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (AllowPrivateAccess), Category= "Grid")
+	UBoxComponent* Box;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (AllowPrivateAccess), Category= "Grid")
+	bool bFlushPreviousDebug = true;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (AllowPrivateAccess), Category= "Grid")
+	bool bDrawDebug = true;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (AllowPrivateAccess), Category= "Grid")
+	bool bDrawLocalDebug = false;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (AllowPrivateAccess), Category= "Grid")
+	bool bGenerateRandomPath = false;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (AllowPrivateAccess), Category= "Grid")
+	bool bGenerateDebug = false;
+
+public:
+	// Sets default values for this actor's properties
+	AKrakenNavBox();
+
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	/*
+	 * Genera la matriz de vóxeles para el sistema de navegación
+	 */
+	UFUNCTION(BlueprintCallable)
+	void GenerateGridInsideBox();
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	FVector GetClosestPointInNavMesh(FVector Point);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	TArray<FVector> GetPath(FVector StartPoint, FVector EndPoint, bool Simplify = true);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	FVector GetRandomPointInNavMesh();
+
+	/*
+	 * Esta función detecta si estamos en el editor y solo si estamos en un nivel y no en la previsualización del blueprint
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	bool IsInWorldEditor();
+
+private:
+	/*
+	 * Llamado desde el generador de malla, comprueba y bloquea todos los vóxeles que estén dentro de una colisión
+	 */
+	void HandleStaticMeshesCollision();
+
+	void HandleBlockingVolumeCollision();
+
+	/*
+	 * Según la orientación de la caja designamos la altura Z relativa correspondiente
+	 */
+	void HandleFloorCollision();
+
+	/*
+	 * Dada una caja de colisión, llena su interior de valores de bloqueo
+	 */
+	void GenerateBoxCollision(FVector BoxCenter, FVector BoxLocalHalfExtent, FTransform Transform);
+
+	/*
+	* Dada una esfera de colisión, llena su interior de valores de bloqueo
+	*/
+	void GenerateSphereCollision(FKSphereElem SphereElem, FTransform Transform);
+
+	void HandleAvoidanceRadius();
+
+	void TestPathfinding();
+
+	void DrawDebugVisualization();
+};
---
+++ Source/HeartWeaver/Public/AI/NavMeshVoxelInfo.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+/*
+ * TODO: BORRAR ESTA CLASE. PROBABLEMENTE NO SE VA A UTILIZAR
+ */
+
+#include "CoreMinimal.h"
+#include "NavMeshVoxelInfo.generated.h"
+
+UENUM(BlueprintType)
+enum class EVoxelValue : uint8 {
+	AVAIBLE = 0,
+	BLOCKED = 1,
+};
+
+USTRUCT(BlueprintType)
+struct FNavMeshVoxelInfo
+{
+	GENERATED_BODY()
+
+	unsigned char Value = 0;
+	
+	// Podemos guardar cualquier otra información del voxel aquí
+};
