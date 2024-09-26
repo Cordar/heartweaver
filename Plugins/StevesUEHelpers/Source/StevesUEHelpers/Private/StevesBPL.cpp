ef6a47b299a50865d97e312fa63193a399ce0879
ChipiDev <josemariaborrella@gmail.com>
2024-09-26 13:29:03 +0200

    Detección de modo de input en interfaces

---
+++ Plugins/StevesUEHelpers/Source/StevesUEHelpers/Private/StevesBPL.cpp
@@ -0,0 +1,50 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StevesBPL.h"
+
+#include "StevesUiHelpers.h"
+#include "StevesUI/StevesUI.h"
+#include "ContentStreaming.h"
+
+void UStevesBPL::SetWidgetFocus(UWidget* Widget)
+{
+	SetWidgetFocusProperly(Widget);
+}
+
+UPanelSlot* UStevesBPL::InsertChildWidgetAt(UPanelWidget* Parent, UWidget* Child, int AtIndex)
+{
+	return StevesUiHelpers::InsertChildWidgetAt(Parent, Child, AtIndex);
+}
+
+FStevesBalancedRandomStream UStevesBPL::MakeBalancedRandomStream(int64 Seed)
+{
+	return FStevesBalancedRandomStream(Seed);
+}
+
+void UStevesBPL::AddViewOriginToStreaming(const FVector& ViewOrigin,
+	float ScreenWidth,
+	float FOV,
+	float BoostFactor,
+	bool bOverrideLocation,
+	float Duration,
+	AActor* ActorToBoost)
+{
+	IStreamingManager::Get().AddViewInformation(ViewOrigin,
+	                                            ScreenWidth,
+	                                            ScreenWidth / FMath::Tan(FMath::DegreesToRadians(FOV * 0.5f)),
+	                                            BoostFactor,
+	                                            bOverrideLocation,
+	                                            Duration,
+	                                            ActorToBoost);
+}
+
+void UStevesBPL::UpdateStreaming(float DeltaTime, bool bBlockUntilDone)
+{
+	FStreamingManagerCollection& SM = IStreamingManager::Get();
+	SM.UpdateResourceStreaming(DeltaTime, true);
+	if (bBlockUntilDone)
+	{
+		SM.BlockTillAllRequestsFinished();
+	}
+}
